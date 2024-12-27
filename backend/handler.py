8f8f8148fa44b60b3a6849efc8c29b200310eeab
sustainablelobster <sustainablelobster@protonmail.com>
2024-12-27 16:48:13 -0500

    add support for other platforms

--- backend/handler.py
+++ backend/handler.py
@@ -14,34 +14,47 @@ SKOPEO = "/opt/skopeo/skopeo"
 SKOPEO_POLICY = "/opt/skopeo/policy.json"
 
 
-def skopeo_copy(image: str) -> None:
+def sanitize(s: str) -> str:
+    return re.sub(r"[^A-Za-z0-9_.-]", "_", s)
+
+
+def get_skopeo_args(image: str, platform: str, tempdir: str, tarball: str) -> list:
+    args = [
+        SKOPEO,
+        "copy",
+        f"docker://{image}",
+        f"docker-archive:{tarball}:{image}",
+        "--tmpdir",
+        tempdir,
+        "--policy",
+        SKOPEO_POLICY,
+    ]
+
+    override_opts = ("--override-os", "--override-arch", "--override-variant")
+    for i, override in enumerate(platform.split("/")):
+        args.append(override_opts[i])
+        args.append(override)
+
+    return args
+
+
+def skopeo_copy(image: str, platform: str) -> None:
     tempdir = tempfile.mkdtemp()
-    sanitized_image = re.sub(r"[^A-Za-z0-9_.-]", "_", image)
-    tarball = os.path.join(tempdir, f"{sanitized_image}.tar")
-    compressed_tarball = f"{tarball}.gz"
+    tarball = os.path.join(tempdir, sanitize(image))
+    if platform != "":
+        tarball += f"_{sanitize(platform)}"
+    tarball += ".tar"
 
-    subprocess.run(
-        [
-            SKOPEO,
-            "--policy",
-            SKOPEO_POLICY,
-            "--tmpdir",
-            tempdir,
-            "copy",
-            f"docker://{image}",
-            f"docker-archive:{tarball}:{image}",
-        ],
-        check=True,
-    )
-
-    subprocess.run([GZIP, tarball], check=True)
+    subprocess.run(get_skopeo_args(image, platform, tempdir, tarball), check=True)
+    subprocess.run((GZIP, tarball), check=True)
+    tarball += ".gz"
 
     try:
         s3_client = boto3.client("s3")
         s3_client.upload_file(
-            compressed_tarball,
+            tarball,
             HARPOON_BUCKET,
-            f"{HARPOON_IMAGES_ROOT}/{os.path.basename(compressed_tarball)}",
+            f"{HARPOON_IMAGES_ROOT}/{os.path.basename(tarball)}",
         )
     finally:
         shutil.rmtree(tempdir)
@@ -50,11 +63,11 @@ def skopeo_copy(image: str) -> None:
 def lambda_handler(event, context):
     try:
         body = json.loads(event["body"])
-        skopeo_copy(body["image"])
+        skopeo_copy(**body)
         return {"statusCode": 200, "body": "success"}
-    except KeyError | json.JSONDecodeError:
+    except (json.JSONDecodeError, KeyError, TypeError):
         return {"statusCode": 400, "body": "bad request"}
     except subprocess.CalledProcessError:
-        return {"statusCode": 404, "body": "image not found"}
+        return {"statusCode": 404, "body": f"image not found"}
     except:
         return {"statusCode": 500, "body": "internal error"}
--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,9 @@
-const API_ENDPOINT = "ENTER API ENDPOINT HERE";
+const API_ENDPOINT = "API ENDPOINT HERE";
 const FORM_ID = "harpoon-form";
 const IMAGE_INPUT_ID = "image";
 const IMAGES_ROOT = "/images";
 const OUTPUT_ID = "output";
+const PLATFORM_ID = "platform";
 
 function disableForm(isDisabled) {
     const form = document.getElementById(FORM_ID);
@@ -16,15 +17,22 @@ function setOutput(message) {
     output.innerHTML = message;
 }
 
+function sanitize(s) {
+    return s.replace(/[^A-Za-z0-9_.-]/g, "_");
+}
+
 async function requestTarballHead(tarballPath) {
     return await fetch(tarballPath, { method: "HEAD" });
 }
 
-async function requestImage(image) {
+async function requestImage(image, platform) {
     return await fetch(API_ENDPOINT, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ image: image })
+        body: JSON.stringify({
+            image: image,
+            platform: platform
+        })
     });
 }
 
@@ -34,12 +42,17 @@ async function formEventListener(event) {
     setOutput("Working...");
 
     const image = document.getElementById(IMAGE_INPUT_ID).value;
-    const sanitizedImage = image.replace(/[^A-Za-z0-9_.-]/g, "_");
-    const tarballPath = `${IMAGES_ROOT}/${sanitizedImage}.tar.gz`;
+    const platform = document.getElementById(PLATFORM_ID).value;
+
+    let tarballPath = `${IMAGES_ROOT}/${sanitize(image)}`;
+    if (platform != "") {
+        tarballPath += `_${sanitize(platform)}`;
+    }
+    tarballPath += ".tar.gz";
 
     let response = await requestTarballHead(tarballPath);
     if (!response.ok) {
-        response = await requestImage(image);
+        response = await requestImage(image, platform);
     }
 
     if (response.ok) {
